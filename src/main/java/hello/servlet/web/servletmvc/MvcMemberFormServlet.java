0cf25e3807805dce6e6ec4a232bd5fa16f25d5b2
kimsanhaa <ttksgk@gmail.com>
2022-04-18 16:10:21 +0900

    servlet MVC를 이용하여 회원추가 / 회원저장 / 회원조회

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.web.servletmvc;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+//WEB-INF안에 있는 jsp들은 Controller를 통해서만  접근이 가능하다.
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath  = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+        // 다른 서블릿이나 JSP로 이동할 수 있는 기능 서버 내부에서 다시 호출이 발생한다.
+
+    }
+}
