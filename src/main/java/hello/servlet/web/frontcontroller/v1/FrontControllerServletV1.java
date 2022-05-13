08da6cc7a5604ea5a806898513fe1043d7349681
kimsanhaa <ttksgk@gmail.com>
2022-05-13 21:43:21 +0900

    프론트 컨트롤러 도입 -V1

--- src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -1,6 +1,8 @@
 package hello.servlet.web.frontcontroller.v1;
 
 import hello.servlet.web.frontcontroller.v1.controller.MemberFormControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberListControllerV1;
+import hello.servlet.web.frontcontroller.v1.controller.MemberSaveControllerV1;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,8 +20,8 @@ public class FrontControllerServletV1  extends HttpServlet {
 
     public FrontControllerServletV1() {
         controllerMap.put("/front-controller/v1/members/new-form", new MemberFormControllerV1());
-        controllerMap.put("/front-controller/v1/members/save", new MemberFormControllerV1());
-        controllerMap.put("/front-controller/v1/members", new MemberFormControllerV1());
+        controllerMap.put("/front-controller/v1/members/save", new MemberSaveControllerV1());
+        controllerMap.put("/front-controller/v1/members", new MemberListControllerV1());
     }
 
     @Override
@@ -29,10 +31,12 @@ public class FrontControllerServletV1  extends HttpServlet {
         String requestURI = request.getRequestURI();
 
         ControllerV1 controller = controllerMap.get(requestURI);
-        if(controller == null){
+        if (controller ==null){
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
+
         controller.process(request, response);
+
     }
 }
