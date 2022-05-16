219798a1cd5b798e64d88bd36fb4f67ff4f11a93
kimsanhaa <ttksgk@gmail.com>
2022-05-16 14:08:16 +0900

    프론트 컨트롤러 도입 -V3

--- src/main/java/hello/servlet/web/frontcontroller/Myview.java
+++ src/main/java/hello/servlet/web/frontcontroller/Myview.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class Myview {
     private String viewPath;
@@ -13,6 +14,12 @@ public class Myview {
         this.viewPath = viewPath;
     }
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        model.forEach((key, value) -> request.setAttribute(key,value));
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
